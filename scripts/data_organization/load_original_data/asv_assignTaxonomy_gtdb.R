000353462aa0fd03157e8b5f4a4fb562c387c111
Efrat Muller <efratmuller@gmail.com>
2023-02-07 22:57:17 +0200

    Update gtdb version

--- scripts/data_organization/load_original_data/asv_assignTaxonomy_gtdb.R
+++ scripts/data_organization/load_original_data/asv_assignTaxonomy_gtdb.R
@@ -2,7 +2,7 @@ library(dada2)
 library(dplyr)
 library(readr)
 
-gtdb.for.dada <- "../references/gtdb/sbdi_16s_gtdb_database/gtdb-sbdi-sativa.r06rs202.assignTaxonomy.fna.gz"
+gtdb.for.dada <- "../references/gtdb/sbdi_16s_gtdb_database/gtdb-sbdi-sativa.r07rs207.1genome.assignTaxonomy.fna.gz"
 datasets <- c("JACOBS_IBD_FAMILIES_2016","HE_INFANTS_MFGM_2019","KIM_ADENOMAS_2020","KOSTIC_INFANTS_DIABETES_2015","POYET_BIO_ML_2019","KANG_AUTISM_2017","WANDRO_PRETERMS_2018")
 
 for (dataset in datasets) {
@@ -36,23 +36,23 @@ for (dataset in datasets) {
     select(asv.id, taxon)
   
   # Read ASV feature table and map to assign taxonomy
-  asvs <- read_delim(asv.file, skip = 1, delim = '\t')
+  asvs <- read_delim(asv.file, skip = 1, delim = '\t', show_col_types = FALSE)
   message(paste("Sanity:",nrow(asvs)))
   asvs <- asvs %>%
     rename(asv.id = 1) %>%
     left_join(tax, by = "asv.id") %>%
     relocate(taxon) %>%
     select(-asv.id) 
-  message(paste("Sanity:",nrow(asvs)))
+  message(paste("Sanity (should be the same as above):",nrow(asvs)))
   
   # Collapse to genus abundances
   genera <- asvs %>%
     group_by(taxon) %>% 
     summarise(across(everything(), sum))
-  message(paste("Sanity:",nrow(genera)))
+  message(paste("Sanity (should be less than above):",nrow(genera)))
   
   # Save
-  write_delim(genera, file.path("../data/original_data", dataset, "feature_table_gtdb.tsv"), delim = "\t")
+  write_delim(genera, file.path("../data/original_data", dataset, "feature_table_gtdb_207.tsv"), delim = "\t")
 }
 
 message("Done")
